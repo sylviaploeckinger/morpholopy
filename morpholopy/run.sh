849c6f48765621f8123c2a42a58005c6501b1bed
Camila Correa <dc-corr1@login7b.pri.cosma7.alces.network>
2021-03-08 12:21:43 +0000

    Listing hawk runs

--- morpholopy/run.sh
+++ morpholopy/run.sh
@@ -2,9 +2,53 @@
 
 # Runs MorpholoPy using the following variables:
 
-folder="/cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188"
-output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/01_reference_L006N0188/output_023"
-snap="23"
+#folder="/snap7/scratch/dp004/dc-chai1/my_cosmological_box/XMAS2020_L006N376_FKIN00_NOEOS_VKICK000SLOPE00NORM00"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/XMAS2020_L006N376_FKIN00_NOEOS_VKICK000SLOPE00NORM00/output_2729"
+#snap="2729"
+
+folder="/snap7/scratch/dp004/dc-chai1/my_cosmological_box/AGN5_L006188_00_UVB_dust1_CR0_G0_shield1"
+output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/AGN5_L006188_00_UVB_dust1_CR0_G0_shield1/output_2729"
+snap="2729"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/01_reference_L006N0188/output_013"
+#snap="13"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/15_alphavir_0p5_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/15_alphavir_0p5_L006N0188/output_023"
+#snap="23"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/39_only_UVB_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/39_only_UVB_L006N0188/output_013"
+#snap="13"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/03_SNE_4e51_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/03_SNE_4e51_L006N0188/output_023"
+#snap="23"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/05_delay_40Myr_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/05_delay_40Myr_L006N0188/output_023"
+#snap="23"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/09_steepEOS_1e3_pressurelaw_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/09_steepEOS_1e3_pressurelaw_L006N0188/output_023"
+#snap="23"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/11_fkin_0p1_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/11_fkin_0p1_L006N0188/output_023"
+#snap="23"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/33_BHboost_densdep_beta0p5_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/33_BHboost_densdep_beta0p5_L006N0188/output_023"
+#snap="23"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/36_SF_efficiency_0p001_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/36_SF_efficiency_0p001_L006N0188/output_023"
+#snap="23"
+
+#folder="/cosma7/data/dp004/dc-chai1/HAWK/29_SNE_densdep_2_4_L006N0188"
+#output="/cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/29_SNE_densdep_2_4_L006N0188/output_023"
+#snap="23"
 
 #folder="/Users/camila/Dropbox/Science-projects/swift-COLIBRE/morphology_estimators/data"
 #output="/Users/camila/Dropbox/Science-projects/swift-COLIBRE/morphology_estimators/data/plots"
