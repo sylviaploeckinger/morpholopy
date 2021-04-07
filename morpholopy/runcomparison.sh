40f1bf6ecb98b69ff55b453fc6e6296e715590ac
Camila Correa <cami.acorrea@gmail.com>
2021-04-07 15:57:27 +0200

    Adding testing run

--- morpholopy/runcomparison.sh
+++ morpholopy/runcomparison.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Runs MorpholoPy using the following variables:
-python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188_CHIMES \
+#python morpholopy.py \
+#-d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188_CHIMES \
+#-s 23 23 \
+#-n Reference Reference+CHIMES \
+#-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/01_reference_L006N0188/comparison
+
+python -i morpholopy.py \
+-d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/39_only_UVB_L006N0188 \
 -s 23 23 \
--n Reference Reference+CHIMES \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/01_reference_L006N0188/comparison
+-n Reference Reference+OnlyUVB \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_only_UVB_01_39
 
