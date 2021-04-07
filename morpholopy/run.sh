9ebbf8c6fc560d24bdbc04af2a1d68f86e79b8a3
Camila Correa <cami.acorrea@gmail.com>
2021-04-07 16:31:48 +0200

    Updating single run

--- morpholopy/run.sh
+++ morpholopy/run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Runs MorpholoPy using the following variables:
-
 python morpholopy.py \
 -d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 \
--s 23 \
+-s 19 \
 -n Reference \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/01_reference_L006N0188/comparison
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/01_reference_L006N0188/output_019
 
 
