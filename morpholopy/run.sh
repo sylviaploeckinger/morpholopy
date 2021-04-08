7af2ece90cdfd3f857eff953aeebd4117875ce98
Camila Correa <cami.acorrea@gmail.com>
2021-04-08 11:56:47 +0200

    Adding BH boots runs

--- morpholopy/run.sh
+++ morpholopy/run.sh
@@ -2,25 +2,25 @@
 
 # Runs MorpholoPy using the following variables:
 python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/19_thermalSNE_2e51_L006N0188  \
+-d /cosma7/data/dp004/dc-chai1/HAWK/07_BHboost_m0p5dex_L006N0188 \
 -s 23 \
--n SNE2e51 \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/19_thermalSNE_2e51_L006N0188/output_023
+-n BHboost \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/07_BHboost_m0p5dex_L006N0188/output_023
 
 python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/19_thermalSNE_2e51_L006N0188  \
+-d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/07_BHboost_m0p5dex_L006N0188  \
 -s 23 23 \
--n Reference SNE2e51 \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_SNE_01_19
+-n Reference BHboost \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_VARY_BH_BOOST_01_07
 
 python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/20_thermalSNE_3e51_L006N0188 \
+-d /cosma7/data/dp004/dc-chai1/HAWK/33_BHboost_densdep_beta0p5_L006N0188 \
 -s 23 \
--n SNE3e51 \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/20_thermalSNE_3e51_L006N0188/output_023
+-n BHboost \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/33_BHboost_densdep_beta0p5_L006N0188/output_023
 
 python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/20_thermalSNE_3e51_L006N0188 \
+-d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/33_BHboost_densdep_beta0p5_L006N0188 \
 -s 23 23 \
--n Reference SNE3e51 \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_SNE_01_20
+-n Reference BHboostDensDep \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_VARY_BH_BOOST_INCL_DENS_DEP_01_33
