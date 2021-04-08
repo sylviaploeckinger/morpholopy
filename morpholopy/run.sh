264c1526764706e1dae529e17826e47959e48f88
Camila Correa <cami.acorrea@gmail.com>
2021-04-08 11:24:41 +0200

    Adding new runs

--- morpholopy/run.sh
+++ morpholopy/run.sh
@@ -2,25 +2,25 @@
 
 # Runs MorpholoPy using the following variables:
 python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/03_SNE_4e51_L006N0188  \
+-d /cosma7/data/dp004/dc-chai1/HAWK/19_thermalSNE_2e51_L006N0188  \
 -s 23 \
--n SNE4e51 \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/03_SNE_4e51_L006N0188/output_023
+-n SNE2e51 \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/19_thermalSNE_2e51_L006N0188/output_023
 
 python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/03_SNE_4e51_L006N0188  \
+-d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/19_thermalSNE_2e51_L006N0188  \
 -s 23 23 \
--n Reference SNE4e51 \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_SNE_01_03
+-n Reference SNE2e51 \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_SNE_01_19
 
 python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/05_delay_40Myr_L006N0188 \
+-d /cosma7/data/dp004/dc-chai1/HAWK/20_thermalSNE_3e51_L006N0188 \
 -s 23 \
--n SNTimeDelay \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/05_delay_40Myr_L006N0188/output_023
+-n SNE3e51 \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/20_thermalSNE_3e51_L006N0188/output_023
 
 python morpholopy.py \
--d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/05_delay_40Myr_L006N0188 \
+-d /cosma7/data/dp004/dc-chai1/HAWK/01_reference_L006N0188 /cosma7/data/dp004/dc-chai1/HAWK/20_thermalSNE_3e51_L006N0188 \
 -s 23 23 \
--n Reference SNTimeDelay \
--o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_VARY_SNE_TIME_DELAY_01_05
+-n Reference SNE3e51 \
+-o /cosma/home/www/swift.dur.ac.uk/public_html/COLIBREPlots/ccorrea/MorpholoPy/HAWK/comparisons/z0.0_SNE_01_20
