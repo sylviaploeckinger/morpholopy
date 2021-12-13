921d2bd9620305cac7bb9dd94b0a8ec24c7b8382
James <j.w.trayford@durham.ac.uk>
2021-12-13 16:43:36 +0000

    add GALAH data and plot it, use dictionary for easier iteration, fixes to get script to run

--- morpholopy/plotter/stellar_abundances.py
+++ morpholopy/plotter/stellar_abundances.py
@@ -1,6 +1,7 @@
 import matplotlib.pylab as plt
 from matplotlib.pylab import rcParams
 import numpy as np
+import h5py as h5
 from tqdm import tqdm
 
 def load_satellites_data_Mg_Fe():
@@ -144,6 +145,23 @@ def load_MW_data():
 
     return FeH_MW, OFe_MW
 
+def load_GALAH_data():
+    file = "./plotter/obs_data/Buder21_data.hdf5"
+    GALAH_dataobj = h5.File(file, 'r')
+    return GALAH_dataobj
+
+def plot_GALAH_data(element, galah_edges, galah_data):
+    obs_plane = np.array(galah_data[f"{element}_enrichment_vs_Fe_abundance"]).T
+    obs_plane[obs_plane < 10] = None
+    
+    contour = plt.contour(np.log10(obs_plane), origin='lower',
+                         extent=[galah_edges[0], galah_edges[-1],
+                                 galah_edges[0], galah_edges[-1]],
+                          zorder=100,
+                          cmap='winter')
+    contour.collections[0].set_label('GALAH DR3')
+
+
 def load_MW_data_with_Mg_Fe():
     file = './plotter/obs_data/MW.txt'
     data = np.loadtxt(file)
@@ -151,27 +169,43 @@ def load_MW_data_with_Mg_Fe():
     MgFe_mw = data[:, 1]
     return FeH_mw, MgFe_mw
 
-def compute_ratios(Hydrogen_fraction, Magnesium_fraction, Oxygen_fraction, Iron_fraction):
-
+def compute_ratios(Hydrogen_fraction, Magnesium_fraction, Oxygen_fraction, Iron_fraction,
+                   Carbon_fraction, Silicon_fraction, Europium_fraction):
+    
     mp_in_cgs = 1.6737236e-24
     mH_in_cgs = 1.00784 * mp_in_cgs
     mFe_in_cgs = 55.845 * mp_in_cgs
     mO_in_cgs = 15.999 * mp_in_cgs
     mMg_in_cgs = 24.305 * mp_in_cgs
+    mC_in_cgs =  12.0107 * mp_in_cgs
+    mSi_in_cgs = 28.0855 * mp_in_cgs
+    mEu_in_cgs = 151.964 * mp_in_cgs
+                
 
     # Asplund et al. (2009)
     Fe_H_Sun = 7.5
     O_H_Sun = 8.69
     Mg_H_Sun = 7.6
+    C_H_Sun = 8.43
+    Si_H_Sun = 7.51
+    Eu_H_Sun = 0.52
 
     O_Fe_Sun = O_H_Sun - Fe_H_Sun - np.log10(mFe_in_cgs / mO_in_cgs)
     Mg_Fe_Sun = Mg_H_Sun - Fe_H_Sun - np.log10(mFe_in_cgs / mMg_in_cgs)
+    C_Fe_Sun = C_H_Sun - Fe_H_Sun - np.log10(mFe_in_cgs / mC_in_cgs)
+    Si_Fe_Sun = Si_H_Sun - Fe_H_Sun - np.log10(mFe_in_cgs / mSi_in_cgs)
+    Eu_Fe_Sun = Eu_H_Sun - Fe_H_Sun - np.log10(mFe_in_cgs / mEu_in_cgs)
     Fe_H_Sun = Fe_H_Sun - 12.0 - np.log10(mH_in_cgs / mFe_in_cgs)
 
     Fe_H = np.log10(Iron_fraction / Hydrogen_fraction) - Fe_H_Sun
     O_Fe = np.log10(Oxygen_fraction / Iron_fraction) - O_Fe_Sun
     Mg_Fe = np.log10(Magnesium_fraction / Iron_fraction) - Mg_Fe_Sun
+    C_Fe = np.log10(Carbon_fraction / Iron_fraction) - C_Fe_Sun
+    Si_Fe = np.log10(Silicon_fraction / Iron_fraction) - Si_Fe_Sun
+    Eu_Fe = np.log10(Europium_fraction / Iron_fraction) - Eu_Fe_Sun
 
+    # print(C_Fe, Si_Fe, Eu_Fe)
+    
     # Let's set lower and upper limits:
     Fe_H[Iron_fraction == 0] = -7  # set lower limit
     Fe_H[Fe_H < -7] = -7  # set lower limit
@@ -181,7 +215,18 @@ def compute_ratios(Hydrogen_fraction, Magnesium_fraction, Oxygen_fraction, Iron_
     O_Fe[Iron_fraction == 0] = -2  # set lower limit
     O_Fe[Oxygen_fraction == 0] = -2  # set lower limit
     O_Fe[O_Fe < -2] = -2  # set lower limit
-    return Fe_H, O_Fe, Mg_Fe
+    C_Fe[Iron_fraction == 0] = -2  # set lower limit
+    C_Fe[Carbon_fraction == 0] = -2  # set lower limit
+    C_Fe[C_Fe < -2] = -2  # set lower limit
+    Si_Fe[Iron_fraction == 0] = -2  # set lower limit
+    Si_Fe[Silicon_fraction == 0] = -2  # set lower limit
+    Si_Fe[Si_Fe < -2] = -2  # set lower limit
+    Eu_Fe[Iron_fraction == 0] = -2  # set lower limit
+    Eu_Fe[Europium_fraction == 0] = -2  # set lower limit
+    Eu_Fe[Eu_Fe < -2] = -2  # set lower limit
+
+    return {'Fe_H':Fe_H, 'O_Fe':O_Fe, 'Mg_Fe':Mg_Fe,
+            'C_Fe':C_Fe, 'Si_Fe':Si_Fe, 'Eu_Fe':Eu_Fe}
 
 def calculate_abundaces_from_MW_type_galaxies(sim_info):
 
@@ -195,27 +240,37 @@ def calculate_abundaces_from_MW_type_galaxies(sim_info):
     Iron_fraction = []
     Magnesium_fraction = []
     Hydrogen_fraction = []
+    Carbon_fraction = []
+    Silicon_fraction = []
+    Europium_fraction = []
 
     for i in tqdm(range(len(sample))):
 
         gas_data, stars_data = sim_info.make_particle_data(
             halo_id=sim_info.halo_data.halo_ids[sample[i]]
         )
-
+        
         O_stars = stars_data[:, 12]
         Fe_stars = stars_data[:, 13]
         Mg_stars = stars_data[:, 14]
         H_stars = stars_data[:, 15]
+        C_stars = stars_data[:, 16]
+        Si_stars = stars_data[:, 17]
+        Eu_stars = stars_data[:, 18]
 
         Oxygen_fraction = np.append(Oxygen_fraction, O_stars)
         Iron_fraction = np.append(Iron_fraction, Fe_stars)
         Magnesium_fraction = np.append(Magnesium_fraction, Mg_stars)
         Hydrogen_fraction = np.append(Hydrogen_fraction, H_stars)
+        Carbon_fraction = np.append(Carbon_fraction, C_stars)
+        Silicon_fraction = np.append(Silicon_fraction, Si_stars)
+        Europium_fraction = np.append(Europium_fraction, Eu_stars)
 
-    Fe_H, O_Fe, Mg_Fe = compute_ratios(Hydrogen_fraction, Magnesium_fraction,
-                                       Oxygen_fraction, Iron_fraction)
-
-    return Fe_H, O_Fe, Mg_Fe
+    ratios = compute_ratios(Hydrogen_fraction, Magnesium_fraction, 
+                            Oxygen_fraction, Iron_fraction,
+                            Carbon_fraction, Silicon_fraction,
+                            Europium_fraction)
+    return ratios
 
 
 def calculate_abundaces_from_satellite_galaxies(sim_info):
@@ -231,6 +286,9 @@ def calculate_abundaces_from_satellite_galaxies(sim_info):
     Iron_fraction = []
     Magnesium_fraction = []
     Hydrogen_fraction = []
+    Carbon_fraction = []
+    Silicon_fraction = []
+    Europium_fraction = []
 
     for i in tqdm(range(len(sample))):
         gas_data, stars_data = sim_info.make_particle_data(
@@ -241,29 +299,45 @@ def calculate_abundaces_from_satellite_galaxies(sim_info):
         Fe_stars = stars_data[:, 13]
         Mg_stars = stars_data[:, 14]
         H_stars = stars_data[:, 15]
+        C_stars = stars_data[:, 16]
+        Si_stars = stars_data[:, 17]
+        Eu_stars = stars_data[:, 18]
 
         Oxygen_fraction = np.append(Oxygen_fraction, O_stars)
         Iron_fraction = np.append(Iron_fraction, Fe_stars)
         Magnesium_fraction = np.append(Magnesium_fraction, Mg_stars)
         Hydrogen_fraction = np.append(Hydrogen_fraction, H_stars)
+        Carbon_fraction = np.append(Carbon_fraction, C_stars)
+        Silicon_fraction = np.append(Silicon_fraction, Si_stars)
+        Europium_fraction = np.append(Europium_fraction, Eu_stars)
 
-    Fe_H, O_Fe, Mg_Fe = compute_ratios(Hydrogen_fraction, Magnesium_fraction,
-                                       Oxygen_fraction, Iron_fraction)
-
-    return Fe_H, O_Fe, Mg_Fe
+    ratios = compute_ratios(Hydrogen_fraction, Magnesium_fraction, 
+                            Oxygen_fraction, Iron_fraction,
+                            Carbon_fraction, Silicon_fraction,
+                            Europium_fraction)
+    return ratios
 
 
 def plot_stellar_abundances(sim_info, output_path):
 
     # Look for abundance ratios from COLIBRE snaps:
-    Fe_H, O_Fe, Mg_Fe = calculate_abundaces_from_MW_type_galaxies(sim_info)
-
+    ratios_MW = calculate_abundaces_from_MW_type_galaxies(sim_info)
+    O_Fe = ratios_MW['O_Fe']
+    Mg_Fe = ratios_MW['Mg_Fe']
+    Fe_H = ratios_MW['Fe_H']
+    
     # Load MW data:
     FeH_MW, OFe_MW = load_MW_data()
 
+    # Load MW data
+    GALAHdata =  load_GALAH_data()
+    galah_edges = np.array(GALAHdata["abundance_bin_edges"])
+    
     # Load MW data:
-    Fe_H_sat, O_Fe_sat, Mg_Fe_sat = calculate_abundaces_from_satellite_galaxies(sim_info)
-
+    ratios_sat = calculate_abundaces_from_satellite_galaxies(sim_info)
+    Fe_H_sat = ratios_sat['Fe_H']
+    O_Fe_sat = ratios_sat['O_Fe']
+    Mg_Fe_sat = ratios_sat['Mg_Fe']
     # Load Satellite data:
     FeH_fornax, OFe_fornax, FeH_sg, OFe_sg, FeH_ca, OFe_ca, FeH_scu, OFe_scu = load_satellites_data()
 
@@ -274,7 +348,7 @@ def plot_stellar_abundances(sim_info, output_path):
         "font.size": 11,
         "font.family": "Times",
         "text.usetex": True,
-        "figure.figsize": (6, 3),
+        "figure.figsize": (7, 3),
         "figure.subplot.left": 0.1,
         "figure.subplot.right": 0.95,
         "figure.subplot.bottom": 0.15,
@@ -295,6 +369,8 @@ def plot_stellar_abundances(sim_info, output_path):
 
     plt.plot(FeH_MW, OFe_MW, '+', color='tab:blue', ms=4, label='MW')
 
+    plot_GALAH_data('O', galah_edges, GALAHdata)
+    
     bins = np.arange(-7.2, 1, 0.2)
     ind = np.digitize(Fe_H, bins)
     xm = [np.median(Fe_H[ind == i]) for i in range(1, len(bins)) if len(Fe_H[ind == i]) > 10]
@@ -319,6 +395,8 @@ def plot_stellar_abundances(sim_info, output_path):
     plt.plot(FeH_fornax, OFe_fornax, 'o', color='tab:orange', ms=4, label='Fornax')
     plt.plot(FeH_sg, OFe_sg, 'v', ms=4, color='crimson', label='Sagittarius')
 
+    plot_GALAH_data('O', galah_edges, GALAHdata)
+    
     bins = np.arange(-7.2, 1, 0.2)
     ind = np.digitize(Fe_H_sat, bins)
     xm = [np.median(Fe_H_sat[ind == i]) for i in range(1, len(bins)) if len(Fe_H_sat[ind == i]) > 10]
@@ -350,7 +428,8 @@ def plot_stellar_abundances(sim_info, output_path):
     plt.plot(Fe_H, Mg_Fe, 'o', ms=0.5, color='grey')
 
     plt.plot(FeH_MW, MgFe_MW, '+', color='orange', ms=4, label='MW')
-
+    plot_GALAH_data('Mg', galah_edges, GALAHdata)
+    
     bins = np.arange(-7.2, 1, 0.2)
     ind = np.digitize(Fe_H, bins)
     xm = [np.median(Fe_H[ind == i]) for i in range(1, len(bins)) if len(Fe_H[ind == i]) > 10]
@@ -375,6 +454,8 @@ def plot_stellar_abundances(sim_info, output_path):
     plt.plot(FeH_fornax, MgFe_fornax, '<', color='royalblue', ms=4, label='Fornax')
     plt.plot(FeH_sagittarius, MgFe_sagittarius, '*', ms=4, color='lightblue', label='Sagittarius')
 
+    plot_GALAH_data('Mg', galah_edges, GALAHdata)
+    
     bins = np.arange(-7.2, 1, 0.2)
     ind = np.digitize(Fe_H, bins)
     xm = [np.median(Fe_H[ind == i]) for i in range(1, len(bins)) if len(Fe_H[ind == i]) > 10]
@@ -389,3 +470,28 @@ def plot_stellar_abundances(sim_info, output_path):
                columnspacing=0.02)
 
     plt.savefig(f"{output_path}/Mg_Fe_" + sim_info.simulation_name + ".png", dpi=200)
+
+    # make remaining plots with just GALAH data (Buder+21)
+    for el in ['C','Si','Eu']:
+        fig = plt.figure(figsize=(3.8,3))
+        ax = plt.subplot(1, 1, 1)
+        plt.grid("True")
+
+        plt.plot(Fe_H, ratios_MW[f'{el}_Fe'], 'o', ms=0.5, color='grey')
+        plot_GALAH_data(el, galah_edges, GALAHdata)
+
+        bins = np.arange(-7.2, 1, 0.2)
+        ind = np.digitize(Fe_H, bins)
+        xm = [np.median(Fe_H[ind == i]) for i in range(1, len(bins)) if len(Fe_H[ind == i]) > 10]
+        ym = [np.median(ratios_MW[f'{el}_Fe'][ind == i]) for i in range(1, len(bins)) if len(ratios_MW[f'{el}_Fe'][ind == i]) > 10]
+        plt.plot(xm, ym, '-', lw=1.5, color='black',label=sim_info.simulation_name)
+
+        plt.xlabel("[Fe/H]", labelpad=2)
+        plt.ylabel(f"[{el}/Fe]", labelpad=2)
+        plt.text(-3.8, 1.2, "MW-type galaxies")
+        plt.axis([-4, 1, -2, 1.5])
+        plt.legend(loc=[0, 0.02], labelspacing=0.1, handlelength=1.5, handletextpad=0.1, frameon=False, ncol=1,
+                   columnspacing=0.02)
+        plt.tight_layout()
+        plt.savefig(f"{output_path}/{el}_Fe_" + sim_info.simulation_name + ".png", dpi=200)
+
